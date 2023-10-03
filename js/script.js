9bddc99f470ac9ff1b5e57c06d741d329e3f0c8e
aldoaquino451 <138690750+aldoaquino451@users.noreply.github.com>
2023-10-03 13:07:12 +0200

    [add] evento click sulla chat thumb

--- js/script.js
+++ js/script.js
@@ -1,6 +1,4 @@
 /*
-  - cicliamo nell'html tutti gli oggetti contenuti in messages con v-for così tramite data e message assegniamo i valori del testo e della data di invio mentre con status verifichiamo chi ha inviato il messaggio e quindi il colore di background
-  - tutte gli elementi chat dovranno essere nascosti tranne il primo 
   - alle thumb chat aggiungiamo una funzione al click, questa funzione ci permetterà di aggiugnere la classe hide all'elemento chat corrispondente e attiverà la thumb (modificando il background)
   - assegnamo all'input un v-model e dentro una variabile per salvare il dato inserito
       - con una funzione che si attiva con il tasto enter pushamo un nuovo oggetto dentro l'array messages corrispondente al contatto della chat corrispondete
@@ -19,14 +17,21 @@ createApp ({
     return {
       saluto,
       contacts,
-      
+      counter: 0,
     }
   },
 
   methods: {
     salutare() {
       console.log(this.saluto);
+    },
+
+    activeThumb(index) {
+      this.contacts[this.counter].visible = !this.contacts[this.counter].visible;
+      this.counter = index;
+      this.contacts[this.counter].visible = !this.contacts[this.counter].visible;
     }
+
   },
 
   mounted() {
